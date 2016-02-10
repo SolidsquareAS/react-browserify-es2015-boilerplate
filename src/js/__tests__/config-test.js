1f65353b1aa0a257ae95c4ab9810f7d25021c2de
Fredrik Oterholt <fredrik.oterholt@solidsquare.no>
2016-02-10 19:25:01 +0100

    Added jest.

---
+++ src/js/__tests__/config-test.js
@@ -0,0 +1,19 @@
+jest.dontMock('../config');
+
+import config from '../config';
+
+describe('config', () => {
+    const defaultPort = 5000;
+    it('should be defined', () => {
+        expect(config).toBeDefined();
+    });
+    it('should have the default port', () => {
+        expect(config.port).toEqual(defaultPort);
+    });
+    it('should have default host localhost with a port', () => {
+        expect(config.host).toMatch(/^localhost:\d+/);
+    });
+    it('should have default host with default port', () => {
+        expect(config.host).toEqual('localhost:' + defaultPort);
+    });
+});
