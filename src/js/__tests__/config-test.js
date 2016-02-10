7f0567ae6860e60c39efca2cbe0c9b63a8c5ef3a
Fredrik Oterholt <fredrik.oterholt@solidsquare.no>
2016-02-10 21:05:07 +0100

    Changed the indent in eslintrc to match .editorconfig.

--- src/js/__tests__/config-test.js
+++ src/js/__tests__/config-test.js
@@ -3,17 +3,17 @@ jest.dontMock('../config');
 const config = require('../config');
 
 describe('config', () => {
-    const defaultPort = 5000;
-    it('should be defined', () => {
-        expect(config).toBeDefined();
-    });
-    it('should have the default port', () => {
-        expect(config.port).toEqual(defaultPort);
-    });
-    it('should have default host localhost with a port', () => {
-        expect(config.host).toMatch(/^localhost:\d+/);
-    });
-    it('should have default host with default port', () => {
-        expect(config.host).toEqual('localhost:' + defaultPort);
-    });
+  const defaultPort = 5000;
+  it('should be defined', () => {
+    expect(config).toBeDefined();
+  });
+  it('should have the default port', () => {
+    expect(config.port).toEqual(defaultPort);
+  });
+  it('should have default host localhost with a port', () => {
+    expect(config.host).toMatch(/^localhost:\d+/);
+  });
+  it('should have default host with default port', () => {
+    expect(config.host).toEqual('localhost:' + defaultPort);
+  });
 });
