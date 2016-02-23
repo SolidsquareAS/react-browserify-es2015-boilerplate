906efd72c0c1556211a250602069b7541538c2f1
Fredrik Oterholt <fredrik.oterholt@solidsquare.no>
2016-02-23 16:41:59 +0100

    Fixed routes.

--- src/private/js/routes.js
+++ src/private/js/routes.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { Route } from 'react-router';
 import App from './components/App';
 
-export const Routes = () => {
-  return (
-    <Route path="/" component={App}/>
-  );
-};
-
-export default {Routes};
+export default (
+  <Route path="/" component={App}/>
+);
