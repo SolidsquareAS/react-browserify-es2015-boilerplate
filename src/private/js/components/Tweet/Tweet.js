5047449feddf4982bfe702f7f20da5c5c322e219
Fredrik Oterholt <fredrik.oterholt@solidsquare.no>
2016-02-23 16:25:41 +0100

    Added compoents for displaying tweets.

---
+++ src/private/js/components/Tweet/Tweet.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+
+export const Tweet = (id, text, timestamp) => (
+  <li>{id} - {timestamp} - {text}</li>
+);
+
+Tweet.propTypes = {
+  id: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  timestamp: PropTypes.number.isRequired
+};
---
+++ src/private/js/components/Tweets/Tweets.js
@@ -0,0 +1,18 @@
+import React, { PropTypes } from 'react';
+import Tweet from './../Tweet';
+
+export const Tweets = (tweets) => (
+  <ul>
+    {tweets.map(tweet =>
+      <Tweet key={tweet.id} {...tweet}/>
+    )}
+  </ul>
+);
+
+Tweets.propTypes = {
+  tweets: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    timestamp: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired
+  }).isRequired).isRequired
+};
