58cd74befb9b926cb97fae7b06b053bff3a2b4f5
Fredrik Oterholt <fredrik.oterholt@solidsquare.no>
2016-02-23 16:24:58 +0100

    Added redux and redux router.

--- src/private/js/routes.js
+++ src/private/js/routes.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router';
-import HomePage from './components/HomePage';
+import App from './components/App';
 
-module.exports = (
-  <Route path="/" component={HomePage}/>
-);
+export const Routes = () => {
+  return (
+    <Route path="/" component={App}/>
+  );
+};
+
+export default {Routes};
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
-
 import { Router, browserHistory } from 'react-router';
+import { syncHistory, routeReducer } from 'react-router-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
 import routes from '../../private/js/routes';
+import reducers from './../../private/js/components/TwitterStore/reducers';
+
+const reducer = combineReducers(Object.assign({}, reducers, {
+  routing: routeReducer
+}));
+
+const middleware = syncHistory(browserHistory);
+const createStoreWithMiddleware = applyMiddleware(middleware)(createStore);
+
+let store = createStoreWithMiddleware(reducer);
+middleware.listenForReplays(store);
 
 render((
-  <Router history={browserHistory} routes={routes} />
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes}/>
+  </Provider>
 ), document.getElementById('reactContainer'));
